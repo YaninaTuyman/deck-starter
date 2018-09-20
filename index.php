5b723d5ffe8b21cf11b37f34c0510d188ad769e1
Yanina Tuyman <tuym0001@algonquinlive.com>
2018-09-20 03:25:14 -0400

    Completed the Dominoes with PHP Assignment

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
-/*
-Task 1
-Loop thouht the suits
- Loop thouht the ranks
-  Create card from suit and rank
-  
-  Task 2
-  Loop 52 times
-   Get random suit
-   Get random rank
-    Create card from suit and rank
+/* Add a number class to the domino section 
+based on the number of dots created. 
  */
 $numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,47 +10,41 @@ $numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Deck</title>
-  <link href="https://fonts.googleapis.com/css?family=BioRhyme:300" rel="stylesheet">
+  <title>DOMinoes with PHP</title>
   <link rel="stylesheet" href="dominoes.css">
 </head>
 <body>
- 
-    
-
+  <h1>Yanina Tuyman's Dominoes</h1>
+  <!-- This is a main container which contane all 100 dominoes -->
   <div id="dominoes" class="dominoes">
-      
-      <?php for ($x = 0; $x <= 100; $x++) { ?>
-      
-   <?php 
-    $randnum1 = rand(0, 6);
-    $randnum2 = rand(0, 6);
-
-    
-    ?>
-      
-    <div class="domino">
-        
-            <div class="dots $numbers[randnum1]">
-			<?php for ($i = 0; $i < $randnum1; $i++) { ?>
-                <div class="dot"></div>
-            <?php } ?>
-		</div>
-        
+  <!-- Loop 100 times -->
+  <?php for ($x = 0; $x <= 100; $x++) { ?>
+      <!-- Create two random numbers for two sections of dominoes -->
+      <?php 
+      $randnum1 = rand(0, 6);
+      $randnum2 = rand(0, 6);
+      ?>
+       <!-- The domino itself -->  
+       <div class="domino">
+       <!-- Inside the domino we have two sections -->   
+        <div class="dots $numbers[randnum1]">
+         <?php for ($i = 0; $i < $randnum1; $i++) { ?>
+              <!-- The dot inside the domino --> 
+              <div class="dot"></div>
+            <?php 
+          } ?>
+       </div>
+        <!-- The second section of the domino -->    
         <div class="dots $numbers[randnum2]">
-			<?php for ($i = 0; $i < $randnum2; $i++) { ?>
-                <div class="dot"></div>
-            <?php } ?>
-		</div>
-            
+         <?php for ($i = 0; $i < $randnum2; $i++) { ?>
+              <!-- The dot inside the domino -->
+              <div class="dot"></div>
+            <?php 
+          } ?>
+       </div>
     </div>
-      
- 
- <?php } ?>
-      
+    <?php 
+  } ?>
   </div>
-    
-    
 </body>
-    
 </html>
